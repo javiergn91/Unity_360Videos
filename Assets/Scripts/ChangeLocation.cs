3ce34ba2c17b752694a0148af6f5b05d0fc89013
Javier Gonzalez <jgonzalez@dparadig.com>
2018-11-03 23:45:30 -0300

    Commit

---
+++ Assets/Scripts/ChangeLocation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeLocation : MonoBehaviour
+{
+    public Image filled;
+    public Transform newLocation;
+
+    private float timeToLoad = 1;
+    private bool isLoading = false;
+    private float currTime = 0;
+
+    public void StartLoading()
+    {
+        Debug.Log("StartLoading");
+        isLoading = true;
+        currTime = 0;
+    }
+
+    public void StopLoading()
+    {
+        Debug.Log("StopLoading");
+        isLoading = false;
+        filled.fillAmount = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if(isLoading)
+        {
+            currTime += Time.deltaTime;
+
+            float t = currTime / timeToLoad;
+
+            filled.fillAmount = t;
+
+            if(t >= 1)
+            {
+                Player.Instance.MoveTo(newLocation);
+                isLoading = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private static Player instance;
+
+    public static Player Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<Player>();
+            }
+
+            return instance;
+        }
+    }
+
+    public void MoveTo(Transform end)
+    {
+        Debug.Log("MoveTo");
+        transform.position = end.position;
+    }
+}
